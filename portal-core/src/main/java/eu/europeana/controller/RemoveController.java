ca84e821a89a9114bc15812b6ede2074a10ea154
geralddejong@gmail.com <geralddejong@gmail.com@3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8>
2009-11-05 11:37:06 +0000

    #304 a more clever way to distinguish between what is to be removed.
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@322 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

--- portal-core/src/main/java/eu/europeana/controller/RemoveController.java
+++ portal-core/src/main/java/eu/europeana/controller/RemoveController.java
@@ -4,9 +4,11 @@ import eu.europeana.controller.util.ControllerUtil;
 import eu.europeana.database.StaticInfoDao;
 import eu.europeana.database.UserDao;
 import eu.europeana.database.domain.CarouselItem;
+import eu.europeana.database.domain.SavedItem;
+import eu.europeana.database.domain.SavedSearch;
 import eu.europeana.database.domain.SearchTerm;
+import eu.europeana.database.domain.SocialTag;
 import eu.europeana.database.domain.User;
-
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -23,6 +25,7 @@ public class RemoveController extends AbstractAjaxController {
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
     }
+
     public void setStaticInfoDao(StaticInfoDao staticInfoDao) {
         this.staticInfoDao = staticInfoDao;
     }
@@ -35,24 +38,56 @@ public class RemoveController extends AbstractAjaxController {
             throw new IllegalArgumentException("Expected 'className' and 'id' parameters!");
         }
         Long id = Long.valueOf(idString);
-        Class clazz = Class.forName("eu.europeana.database.domain."+className);
 
-        //find object to be removed
-
-        //check that the logged in user is the owner of the object
-
-        //do remove
-        if (clazz == SearchTerm.class) {
-            user = staticInfoDao.removeSearchTerm(user, id);
-        }
-        else if (clazz == CarouselItem.class) {
-            user = staticInfoDao.removeCarouselItem(user, id);
-        }
-        else {
-            user = userDao.remove(user, clazz, id);
+        Class clazz = Class.forName("eu.europeana.database.domain." + className); // todo: remove later
+        switch (findRemovable(className)) {
+            case CAROUSEL_ITEM:
+                user = staticInfoDao.removeCarouselItem(user, id);
+                break;
+            case SAVED_ITEM:
+                user = userDao.remove(user, clazz, id);
+                break;
+            case SAVED_SEARCH:
+                user = userDao.remove(user, clazz, id);
+                break;
+            case SEARCH_TERM:
+                user = staticInfoDao.removeSearchTerm(user, id);
+                break;
+            case SOCIAL_TAG:
+                user = userDao.remove(user, clazz, id);
+                break;
+            default:
+                throw new IllegalArgumentException("Unhandled removable");
         }
 
         ControllerUtil.setUser(user);
         return true;
+    }
+
+    private Removable findRemovable(String className) {
+        for (Removable removable : Removable.values()) {
+            if (removable.matches(className)) {
+                return removable;
+            }
+        }
+        throw new IllegalArgumentException("Unable to find removable class with name "+className);
+    }
+
+    private enum Removable {
+        SEARCH_TERM(SearchTerm.class),
+        CAROUSEL_ITEM(CarouselItem.class),
+        SAVED_ITEM(SavedItem.class),
+        SAVED_SEARCH(SavedSearch.class),
+        SOCIAL_TAG(SocialTag.class);
+
+        private String className;
+
+        private Removable(Class<?> clazz) {
+            this.className = clazz.getName().substring(clazz.getName().lastIndexOf('.') + 1);
+        }
+
+        public boolean matches(String className) {
+            return this.className.equals(className);
+        }
     }
 }
