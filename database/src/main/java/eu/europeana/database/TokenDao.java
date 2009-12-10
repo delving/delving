71e8e6bc4ac0e67d8bc380612880b66a58dee2c1
cesare.concordia@isti.cnr.it <cesare.concordia@isti.cnr.it@3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8>
2009-12-10 11:53:39 +0000

    (ticket #449) moving methods from portal-core classes into database module, in progress, see comment 4 in the ticket
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@490 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

--- database/src/main/java/eu/europeana/database/TokenDao.java
+++ database/src/main/java/eu/europeana/database/TokenDao.java
@@ -1,24 +1,14 @@
 package eu.europeana.database;
 
-import java.util.Date;
-import org.springframework.security.ui.rememberme.PersistentRememberMeToken;
+import org.springframework.security.ui.rememberme.PersistentTokenRepository;
 import eu.europeana.database.domain.Token;
 import eu.europeana.query.EuropeanaQueryException;
 
 
 
-public interface TokenDao {
+public interface TokenDao extends PersistentTokenRepository{
 
    
-    public void createNewToken(PersistentRememberMeToken persistentRememberMeToken);
-
-    public void updateToken(String series, String tokenValue, Date lastUsed) ;
-
-    public PersistentRememberMeToken getTokenForSeries(String series);
-   
-    public void removeUserTokens(String username);
-    
-
     public Token createNewToken(String email);
 
    
