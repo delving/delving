571fc03476d2bb11eb465bdabd05c8dd38493731
geralddejong@gmail.com <geralddejong@gmail.com@3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8>
2010-01-07 13:57:39 +0000

    #545 annotation processor is not interface/impl, and in the database appcontext.
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@604 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

--- database/src/main/java/eu/europeana/beans/AnnotationProcessor.java
+++ database/src/main/java/eu/europeana/beans/AnnotationProcessor.java
@@ -1,141 +1,15 @@
 package eu.europeana.beans;
 
-import org.apache.log4j.Logger;
-
 import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 /**
- * Interpret the annotations in the beans which define the search model
+ * Interpret the annotations in the beans which define the search model.  This is the injected interface
  *
  * @author Gerald de Jong geralddejong@gmail.com
  */
 
-public class AnnotationProcessor {
-
-    private Logger log = Logger.getLogger(getClass());
-    private Set<FacetFieldImpl> facetFields = new HashSet<FacetFieldImpl>();
-    private Map<Class<?>, Set<Field>> beanFieldMap = new HashMap<Class<?>, Set<Field>>();
-
-    public AnnotationProcessor(Class<?>... classes) {
-        for (Class<?> c : classes) {
-            processAnnotations(c);
-        }
-    }
-
-    public Set<? extends FacetField> getFacetFields() {
-        return facetFields;
-    }
-
-    public Set<Field> getFields(Class<?> c) {
-        return beanFieldMap.get(c);
-    }
-
-    private void processAnnotations(Class<?> c) {
-        if (!beanFieldMap.containsKey(c)) {
-            Set<Field> fieldSet = new HashSet<Field>();
-            Class<?> clazz = c;
-            while (clazz != Object.class) {
-                for (Field field : clazz.getDeclaredFields()) {
-                    processSolrAnnotation(field);
-                    processEuropeanaAnnotation(field);
-                    fieldSet.add(field);
-                }
-                clazz = clazz.getSuperclass();
-            }
-            beanFieldMap.put(c, fieldSet);
-        }
-    }
-
-    private void processEuropeanaAnnotation(Field field) {
-        Europeana europeana = field.getAnnotation(Europeana.class);
-        if (europeana != null) {
-            logEuropeanaAttributes(field, europeana);
-            if (europeana.facet()) {
-                facetFields.add(new FacetFieldImpl(field));
-            }
-        }
-    }
-
-    private void processSolrAnnotation(Field field) {
-        Solr solr = field.getAnnotation(Solr.class);
-        if (solr != null) {
-            logSolrAttributes(field, solr);
-        }
-    }
-
-    private void logEuropeanaAttributes(Field field, Europeana europeana) {
-        String prefix = "Field ["+field.getName()+"]: ";
-        log.info(prefix + "facetPrefix="+europeana.facetPrefix());
-        log.info(prefix + "briefDoc="+europeana.briefDoc());
-        log.info(prefix + "fullDoc="+europeana.fullDoc());
-        log.info(prefix + "copyField(flag)="+europeana.copyField());
-        log.info(prefix + "facet="+europeana.facet());
-        log.info(prefix + "hidden="+europeana.hidden());
-    }
-
-    private void logSolrAttributes(Field field, Solr solr) {
-        String prefix = "Field ["+field.getName()+"]: ";
-        log.info(prefix + "name="+solr.name());
-        log.info(prefix + "namespace="+solr.namespace());
-        log.info(prefix + "fieldType="+solr.fieldType());
-        log.info(prefix + "indexed="+solr.indexed());
-        log.info(prefix + "multivalued="+solr.multivalued());
-        log.info(prefix + "defaultValue="+solr.defaultValue());
-        log.info(prefix + "omitNorms="+solr.omitNorms());
-        log.info(prefix + "required="+solr.required());
-        log.info(prefix + "stored="+solr.stored());
-        log.info(prefix + "termOffsets="+solr.termOffsets());
-        log.info(prefix + "termPositions="+solr.termPositions());
-        log.info(prefix + "termVectors="+solr.termVectors());
-        log.info(prefix + "compressed="+solr.compressed());
-        for (String copyField : solr.toCopyField()) {
-            log.info(prefix + "copyField="+copyField);
-        }
-    }
-
-    private class FacetFieldImpl implements FacetField {
-        private Field field;
-
-        private FacetFieldImpl(Field field) {
-            this.field = field;
-        }
-
-        @Override
-        public String getPrefix() {
-            return field.getAnnotation(Europeana.class).facetPrefix();
-        }
-
-        @Override
-        public String getName() {
-            String name = field.getAnnotation(org.apache.solr.client.solrj.beans.Field.class).value();
-            if (!name.equals(org.apache.solr.client.solrj.beans.Field.DEFAULT)) {
-                return name;
-            }
-            else {
-                return field.getName();
-            }
-        }
-
-        @Override
-        public String getFieldNameString() {
-            return getPrefix()+'_'+getName();
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            FacetFieldImpl that = (FacetFieldImpl) o;
-            return !(field != null ? !field.equals(that.field) : that.field != null);
-        }
-
-        @Override
-        public int hashCode() {
-            return field != null ? field.hashCode() : 0;
-        }
-    }
+public interface AnnotationProcessor {
+    Set<? extends FacetField> getFacetFields();
+    Set<Field> getFields(Class<?> c);
 }
--- database/src/test/java/eu/europeana/beans/TestAnnotationProcessor.java
+++ database/src/test/java/eu/europeana/beans/TestAnnotationProcessor.java
@@ -4,6 +4,8 @@ import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,20 +18,21 @@ public class TestAnnotationProcessor {
     private Logger log = Logger.getLogger(getClass());
 
     @Test
-    public void readAnnotations() throws Exception {
-        AnnotationProcessor processor = new AnnotationProcessor(
-                IdBean.class,
-                RequiredBean.class,
-                BriefBean.class,
-                FullBean.class,
-                AllFieldBean.class
-        );
-        assertEquals(9, processor.getFields(BriefBean.class).size());
-        for (Field field : processor.getFields(BriefBean.class)) {
+    public void processThem() throws Exception {
+        List<Class<?>> list = new ArrayList<Class<?>>();
+        list.add(IdBean.class);
+        list.add(RequiredBean.class);
+        list.add(BriefBean.class);
+        list.add(FullBean.class);
+        list.add(AllFieldBean.class);
+        AnnotationProcessorImpl annotationProcessor = new AnnotationProcessorImpl();
+        annotationProcessor.setClasses(list);
+        assertEquals(9, annotationProcessor.getFields(BriefBean.class).size());
+        for (Field field : annotationProcessor.getFields(BriefBean.class)) {
             log.info("BriefBean field "+field.getName());
         }
-        assertEquals(5, processor.getFacetFields().size());
-        for (FacetField ff : processor.getFacetFields()) {
+        assertEquals(5, annotationProcessor.getFacetFields().size());
+        for (FacetField ff : annotationProcessor.getFacetFields()) {
             log.info("facet " + ff.getFieldNameString());
         }
 
