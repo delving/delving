606da95f43555964bb2f9ebc395075a4bdf425e4
sjoerd.siebinga@gmail.com <sjoerd.siebinga@gmail.com@3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8>
2010-02-17 18:24:50 +0000

    #569 fixed issue where in typetabs sometimes query wasn't added to the url
    Requested Url: http://www.europeana.eu/portal/brief-doc.html?query=tabula&qf=TYPE:text&view=list  on host www.europeana.eu.
    
    ==========
    java.lang.StringIndexOutOfBoundsException: String index out of range: -1 at java.lang.String.substring(String.java:1938) at eu.europeana.web.util.ResultPaginationImpl.removePresentationFilters(ResultPaginationImpl.java:87) at eu.europeana.web.util.ResultPaginationImpl.(ResultPaginationImpl.java:68) at eu.europeana.beans.query.BeanQueryModelFactory.createPagination(BeanQueryModelFactory.java:484) at eu.europeana.beans.query.
    
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@941 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

--- core/src/main/java/eu/europeana/web/util/ResultPaginationImpl.java
+++ core/src/main/java/eu/europeana/web/util/ResultPaginationImpl.java
@@ -72,19 +72,22 @@ public class ResultPaginationImpl implements ResultPagination {
         String[] filterQueries = requestQueryString.split("&");
         StringBuilder url = new StringBuilder();
         for (String filterQuery : filterQueries) {
-            if (filterQuery.contains("TYPE:")) {
+            if (filterQuery.startsWith("qf=TYPE:")) {
                 continue;
             }
-            if (filterQuery.contains("tab")) {
+            if (filterQuery.startsWith("tab=")) {
                 continue;
             }
-            if (filterQuery.contains("view")) {
+            if (filterQuery.startsWith("view=")) {
                 continue;
             }
             url.append(filterQuery).append("&");
         }
         String urlString = url.toString().trim();
-        return urlString.substring(0, urlString.length() - 1);
+        if (urlString.endsWith("&")) {
+            urlString = urlString.substring(0, urlString.length() - 1);
+        }
+        return urlString;
     }
 
     private String createQueryForPresentation(SolrQuery solrQuery) {
