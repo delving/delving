6174c4bc5e5448774373f0076a42ad60d45313d8
jacob.lundqvist@gmail.com <jacob.lundqvist@gmail.com@3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8>
2010-02-19 08:26:26 +0000

    #90 aboutus import changes
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@959 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

--- tools/multilingo/import/initial_templates_translations.py
+++ tools/multilingo/import/initial_templates_translations.py
@@ -1,3 +1,33 @@
+"""
+ Copyright 2010 EDL FOUNDATION
+
+ Licensed under the EUPL, Version 1.1 or as soon they
+ will be approved by the European Commission - subsequent
+ versions of the EUPL (the "Licence");
+ you may not use this work except in compliance with the
+ Licence.
+ You may obtain a copy of the Licence at:
+
+ http://ec.europa.eu/idabc/eupl
+
+ Unless required by applicable law or agreed to in
+ writing, software distributed under the Licence is
+ distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied.
+ See the Licence for the specific language governing
+ permissions and limitations under the Licence.
+
+
+ Created by: Jacob Lundqvist (Jacob.Lundqvist@gmail.com)
+
+ 2010-02-18 Not Yet completed...
+
+
+ Tool to import original properties files for all languages into this system
+"""
+
+
 import xml.dom.minidom
 import codecs
 import os
@@ -51,6 +81,7 @@ class TranslateTemplate(object):
         self.po = polib.pofile(self._translation_file_base + '.po')
 
 
+
     def translate(self, key, value):
         if key[-1] == '*':
             key = key[:-1] # strip end
@@ -81,6 +112,8 @@ class TranslateTemplate(object):
             return
         self.po.save()
 
+
+
     def nodevalue(self, main_idx, *indexes):
         try:
             x = self.doc.childNodes[main_idx]
@@ -95,6 +128,8 @@ class TranslateTemplate(object):
             if self.debug < 1:
                 raise e
         return r
+
+
 
     def transl_aboutus(self):
         if self.debug > 1:
@@ -272,5 +307,5 @@ def parse_about_us(lang):
 
 
 
-parse_about_us('de')
+parse_about_us('sv')
 
