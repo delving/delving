0a2772da6930915b0c6b9fbcfba3ce9ca4846ebb
sjoerd.siebinga@gmail.com <sjoerd.siebinga@gmail.com@3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8>
2010-02-27 21:37:28 +0000

    SiteMapController now only provides "92001_Ag_EU_TELtreasures" for testing purposes
    Conflicts:
    
    	portal-full/src/main/java/eu/europeana/web/controller/SitemapController.java
    
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@1015 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

--- portal-full/src/main/java/eu/europeana/web/controller/SitemapController.java
+++ portal-full/src/main/java/eu/europeana/web/controller/SitemapController.java
@@ -24,11 +24,13 @@ package eu.europeana.web.controller;
 import eu.europeana.core.database.DashboardDao;
 import eu.europeana.core.database.domain.EuropeanaCollection;
 import eu.europeana.core.querymodel.query.DocId;
+import eu.europeana.core.querymodel.query.EuropeanaQueryException;
 import eu.europeana.core.querymodel.query.QueryModelFactory;
 import eu.europeana.core.querymodel.query.SiteMapBeanView;
 import eu.europeana.core.util.web.ControllerUtil;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.math.NumberUtils;
+import org.apache.solr.client.solrj.SolrServerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -65,22 +67,82 @@ public class SitemapController {
     @Autowired
     DashboardDao dashboardDao;
 
-    @RequestMapping("/sitemap.xml")
+//    @RequestMapping("/sitemap.xml")
     public ModelAndView handleSitemap(
             @RequestParam(value = "collection", required = false) String collection,
             @RequestParam(value = "page", required = false) String page,
             HttpServletRequest request
-    ) throws Exception {
-        ModelAndView mavPage;
+    ) throws SolrServerException, EuropeanaQueryException {
         String fullDocPageString = "full-doc.html";
         String baseUrl = fullViewUrl;
         if (baseUrl.endsWith(fullDocPageString)) {
             baseUrl = baseUrl.substring(0, fullViewUrl.length() - fullDocPageString.length());
         }
+        ModelAndView mavPage;
         if (collection == null) {
             List<SitemapIndexEntry> entries = new ArrayList<SitemapIndexEntry>();
             // todo maybe this query must be cached because it might be too heavy a query on the database
-            List<EuropeanaCollection> europeanaCollections = dashboardDao.fetchCollections();
+            List<EuropeanaCollection> europeanaCollections = dashboardDao.fetchEnabledCollections();
+            for (EuropeanaCollection europeanaCollection : europeanaCollections) {
+                for (int i = 0; i <= europeanaCollection.getTotalRecords() / MAX_RECORDS_PER_SITEMAP_FILE; i++) {
+                    // add each page of a collection to the index.
+                    entries.add(
+                            new SitemapIndexEntry(
+                                    StringEscapeUtils.escapeXml(String.format("%ssitemap.xml?collection=%s&page=%d", baseUrl, europeanaCollection.getName(), i)),
+                                    europeanaCollection.getCollectionLastModified()));
+                }
+            }
+
+            mavPage = ControllerUtil.createModelAndViewPage("sitemap-index");
+            mavPage.addObject("entries", entries);
+        }
+        else {
+            mavPage = ControllerUtil.createModelAndViewPage("sitemap");
+            mavPage.addObject("fullViewUrl", fullViewUrl);
+
+            // generate sitemap for a collection
+            if (page != null && page.length() > 0 && page.length() < 4 && NumberUtils.isDigits(page)) {
+                int pageInt = Integer.parseInt(page);
+                SiteMapBeanView siteMapBeanView = beanQueryModelFactory.getSiteMapBeanView(collection, MAX_RECORDS_PER_SITEMAP_FILE, pageInt);
+                int maxPageForCollection = siteMapBeanView.getMaxPageForCollection();
+                if (pageInt <= maxPageForCollection) {
+                    List<? extends DocId> list = siteMapBeanView.getIdBeans();
+                    mavPage.addObject("idBeanList", list);
+                }
+            }
+        }
+        return mavPage;
+    }
+
+     /**
+     * This is a test method to determine the load crawl bots put on the system and to see how a test collection
+     * "tel-treasures" is discoverable via the web search engine
+     *
+     * @param collection
+     * @param page
+     * @param request
+     * @return ModelAndView
+     * @throws org.apache.solr.client.solrj.SolrServerException
+     * @throws eu.europeana.core.querymodel.query.EuropeanaQueryException
+     */
+    @RequestMapping("/sitemap.xml")
+    public ModelAndView handleTestSitemap(
+            @RequestParam(value = "collection", required = false) String collection,
+            @RequestParam(value = "page", required = false) String page,
+            HttpServletRequest request
+    ) throws SolrServerException, EuropeanaQueryException {
+        String fullDocPageString = "full-doc.html";
+        String baseUrl = fullViewUrl;
+        if (baseUrl.endsWith(fullDocPageString)) {
+            baseUrl = baseUrl.substring(0, fullViewUrl.length() - fullDocPageString.length());
+        }
+        ModelAndView mavPage;
+        if (collection == null) {
+            List<SitemapIndexEntry> entries = new ArrayList<SitemapIndexEntry>();
+            EuropeanaCollection testCollection = new EuropeanaCollection();
+            testCollection.setName("92001_Ag_EU_TELtreasures ");
+            List<EuropeanaCollection> europeanaCollections = new ArrayList<EuropeanaCollection>();
+            europeanaCollections.add(testCollection);
             for (EuropeanaCollection europeanaCollection : europeanaCollections) {
                 for (int i = 0; i <= europeanaCollection.getTotalRecords() / MAX_RECORDS_PER_SITEMAP_FILE; i++) {
                     // add each page of a collection to the index.
