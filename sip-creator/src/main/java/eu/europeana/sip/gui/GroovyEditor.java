0c3e65e31f0a6f086eea78e100ae0715b104c9e6
geralddejong@gmail.com <geralddejong@gmail.com@3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8>
2010-03-30 10:26:35 +0000

    #970 GroovyEditor now uses jsyntaxpane
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@1329 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

--- sip-creator/src/main/java/eu/europeana/sip/gui/GroovyEditor.java
+++ sip-creator/src/main/java/eu/europeana/sip/gui/GroovyEditor.java
@@ -1,22 +1,24 @@
 package eu.europeana.sip.gui;
 
 import eu.europeana.sip.io.GroovyService;
+import jsyntaxpane.DefaultSyntaxKit;
 import org.apache.log4j.Logger;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComponent;
+import javax.swing.JEditorPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.File;
 
 /**
@@ -31,19 +33,26 @@ public class GroovyEditor extends JPanel implements GroovyService.Listener {
     public final static int VALIDATION_DELAY = 500;
     private final static Logger LOG = Logger.getLogger(GroovyEditor.class.getName());
 
-    private JTextArea codeArea = new JTextArea();
+
+    private JEditorPane codeArea = new JEditorPane();
     private JTextArea outputArea = new JTextArea();
     private CompileTimer compileTimer;
     private GroovyService groovyService;
 
     public GroovyEditor(GroovyService.BindingSource bindingSource) {
         super(new BorderLayout());
-        codeArea.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Code"));
+        DefaultSyntaxKit.initKit();
+        add(createSplitPane());
         outputArea.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Output"));
         this.groovyService = new GroovyService(bindingSource, this);
         this.compileTimer = new CompileTimer();
-        codeArea.getDocument().addDocumentListener(compileTimer);
-        add(createSplitPane());
+        this.codeArea.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent event) {
+                compileTimer.triggerSoon();
+            }
+        });
+        codeArea.setContentType("text/groovy");
     }
 
     public void setGroovyFile(File groovyFile) {
@@ -53,8 +62,12 @@ public class GroovyEditor extends JPanel implements GroovyService.Listener {
     private JComponent createSplitPane() {
         JSplitPane split = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
         JScrollPane scroll;
-        split.setTopComponent(scroll = new JScrollPane(codeArea));
+        JPanel top = new JPanel(new BorderLayout());
+        top.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Code"));
+        top.add(scroll = new JScrollPane(codeArea));
+        split.setTopComponent(top);
         scroll.setPreferredSize(new Dimension(1200, 400));
+        scroll.doLayout();
         split.setBottomComponent(scroll = new JScrollPane(outputArea));
         scroll.setPreferredSize(new Dimension(1200, 400));
         split.setDividerLocation(0.5);
@@ -71,6 +84,7 @@ public class GroovyEditor extends JPanel implements GroovyService.Listener {
             @Override
             public void run() {
                 codeArea.setText(groovyCode);
+                compileTimer.triggerSoon();
             }
         });
     }
@@ -85,7 +99,7 @@ public class GroovyEditor extends JPanel implements GroovyService.Listener {
         });
     }
 
-    private class CompileTimer implements ActionListener, DocumentListener {
+    private class CompileTimer implements ActionListener {
 
         private Timer timer = new Timer(VALIDATION_DELAY, this);
 
@@ -95,22 +109,11 @@ public class GroovyEditor extends JPanel implements GroovyService.Listener {
             groovyService.setGroovyCode(codeArea.getText());
         }
 
-        @Override
-        public void insertUpdate(DocumentEvent e) {
+        public void triggerSoon() {
             timer.restart();
-        }
-
-        @Override
-        public void removeUpdate(DocumentEvent e) {
-            timer.restart();
-        }
-
-        @Override
-        public void changedUpdate(DocumentEvent e) {
         }
 
     }
-
     @Override
     public String toString() {
         return "GroovyEditor";
