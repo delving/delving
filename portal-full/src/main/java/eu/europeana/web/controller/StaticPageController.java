92308ee7d58fde79e3c5ce65814b87b7ee420e25
borys.omelayenko@kb.nl <borys.omelayenko@kb.nl>
2010-04-07 13:52:09 +0000

    #1019, displaying mc static pages (js, css, jpg; no html yet)
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@1452 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

--- portal-full/src/main/java/eu/europeana/web/controller/StaticPageController.java
+++ portal-full/src/main/java/eu/europeana/web/controller/StaticPageController.java
@@ -85,7 +85,7 @@ public class StaticPageController {
      * @throws Exception something went wrong
      */
 
-    @RequestMapping("/mc/css/*.css")
+    @RequestMapping("/mc/css/{pageName}.css")
     public ModelAndView fetchMcCss(
             @PathVariable("pageName") String pageName,
             HttpServletRequest request
@@ -103,7 +103,7 @@ public class StaticPageController {
      * @throws Exception something went wrong
      */
 
-    @RequestMapping("/mc/js/*.js")
+    @RequestMapping("/mc/js/{pageName}.js")
     public ModelAndView fetchMcJs(
             @PathVariable("pageName") String pageName,
             HttpServletRequest request
@@ -121,7 +121,7 @@ public class StaticPageController {
      * @throws Exception something went wrong
      */
 
-    @RequestMapping("/mc/img/*.jpg")
+    @RequestMapping("/mc/img/{pageName}.jpg")
     public ModelAndView fetchMcJpg(
             @PathVariable("pageName") String pageName,
             HttpServletRequest request
