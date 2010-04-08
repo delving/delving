3a160823b4d85f5d6b38b22a4d947889942454a9
geralddejong@gmail.com <geralddejong@gmail.com@3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8>
2010-04-08 14:21:39 +0000

    #163 created an Annotation entity, Dao, etc.  controller in api module, for REST service that should work for AIT's image annotation, and any number of other annotation tools of different kinds.
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@1504 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

--- api/src/main/java/eu/europeana/api/controller/ServiceController.java
+++ api/src/main/java/eu/europeana/api/controller/ServiceController.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2007 EDL FOUNDATION
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they
+ * will be approved by the European Commission - subsequent
+ * versions of the EUPL (the "Licence");
+ * you may not use this work except in compliance with the
+ * Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://ec.europa.eu/idabc/eupl
+ *
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied.
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+
 package eu.europeana.api.controller;
 
 import eu.europeana.core.database.incoming.cache.ItemSize;
@@ -13,7 +34,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.URLEncoder;
 
 /**
