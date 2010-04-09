b40d1257e425842f5cb36735d59f519c4b4d8649
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2010-04-09 14:57:07 +0200

    updated some ScalaTest spec files. Moved AnnotationProcessorSpec to annotation package

---
+++ core/src/test/scala/eu/europeana/core/querymodel/annotation/AnnotationProcessorSpec.scala
@@ -0,0 +1,88 @@
+package eu.europeana.core.querymodel.query
+
+import _root_.eu.europeana.core.querymodel.annotation._
+import _root_.java.util.Date
+import _root_.org.apache.solr.client.solrj.beans.Field
+import _root_.eu.europeana.core.querymodel.beans.{AllFieldBean, FullBean, BriefBean, IdBean}
+import _root_.org.junit.runner.RunWith
+import _root_.org.scalatest.matchers.ShouldMatchers
+import _root_.org.scalatest.Spec
+import _root_.org.scalatest.junit.JUnitRunner
+import collection.mutable.ListBuffer
+
+/**
+ *
+ * @author Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
+ * @since Apr 5, 2010 10:55:58 PM
+ */
+
+@RunWith(classOf[JUnitRunner])
+class AnnotationProcessorSpec extends Spec with ShouldMatchers {
+
+  describe("An AnnotationProcessor") {
+
+    describe("(when it is given a List of annotated classes)") {
+      val beanClasses = new ListBuffer[Class[_]] += ((new IdBean).getClass, (new BriefBean).getClass, (new FullBean).getClass, (new AllFieldBean).getClass)
+      val mockBeans = new ListBuffer[Class[_]] += ((new MockAnnotatedBean).getClass)
+      val processor = new AnnotationProcessorImpl
+      processor setClasses mockBeans
+
+      it("should give EuropeanaBeans for each class") {
+        beanClasses.foreach {
+          beanClass: Class[_] =>
+              println (beanClass.getCanonicalName)
+              println (beanClass.getFields.foreach( println (_)))
+              println (beanClass.getAnnotations.size)
+//            val bean: EuropeanaBean = processor.getEuropeanaBean (beanClass)
+//            bean.getFieldStrings.foreach( println (_) )
+//            europeanaBean.getFields.foreach(field => println (field) )
+        }
+      }
+
+    }
+
+  }
+
+
+
+  /*
+  private Logger log = Logger.getLogger(getClass());
+
+    @Test
+    public void processThem() throws Exception {
+        List<Class<?>> list = new ArrayList<Class<?>>();
+        list.add(IdBean.class);
+        list.add(BriefBean.class);
+        list.add(FullBean.class);
+        list.add(AllFieldBean.class);
+        AnnotationProcessorImpl annotationProcessor = new AnnotationProcessorImpl();
+        annotationProcessor.setClasses(list);
+        EuropeanaBean bean = annotationProcessor.getEuropeanaBean(BriefBean.class);
+        assertEquals(11, bean.getFields().size());
+        for (EuropeanaField field : bean.getFields()) {
+            log.info("BriefBean field "+field.getName());
+        }
+        assertEquals(5, annotationProcessor.getFacetFields().size());
+        for (EuropeanaField ff : annotationProcessor.getFacetFields()) {
+            log.info("facet " + ff.getFieldNameString());
+        }
+
+    }
+    */
+
+}
+
+class MockAnnotatedBean {
+
+  @Europeana(briefDoc = true, id = true)
+  @Solr(namespace = "europeana", name = "uri", multivalued = false, required = true)
+  @Field("europeana_uri")
+  @scala.reflect.BeanProperty
+  val europeanaUri: String = "europeanaUri"
+
+  @Europeana()
+  @Solr(name = "timestamp", multivalued = false, defaultValue = "NOW")
+  @Field("timestamp")
+  @scala.reflect.BeanProperty
+  val timestamp = new Date
+}
--- core/src/test/scala/eu/europeana/core/querymodel/query/SolrQueryUtilSpec.scala
+++ core/src/test/scala/eu/europeana/core/querymodel/query/SolrQueryUtilSpec.scala
@@ -14,4 +14,50 @@ import _root_.org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class SolrQueryUtilSpec extends Spec with ShouldMatchers {
 
+
+  /*
+  @Test
+    public void testPhraseConversions() {
+        SolrQuery solrQuery = new SolrQuery();
+        solrQuery.setFilterQueries(
+                "PROVIDER:The European Library",
+                "LANGUAGE:en"
+        );
+        String[] phrased = SolrQueryUtil.getFilterQueriesAsPhrases(solrQuery);
+        for (String s : phrased) {
+            String after = s.substring(s.indexOf(':') + 1);
+            Assert.assertTrue(after.startsWith("\"") && after.endsWith("\""));
+        }
+        solrQuery.setFilterQueries(phrased);
+        String [] unphrased = SolrQueryUtil.getFilterQueriesWithoutPhrases(solrQuery);
+        for (String s : unphrased) {
+            String after = s.substring(s.indexOf(':') + 1);
+            Assert.assertFalse(after.startsWith("\"") && after.endsWith("\""));
+        }
+    }
+
+    @Test
+    public void testOrBasedFacetQueries() throws Exception {
+        SolrQuery solrQuery = new SolrQuery();
+        solrQuery.setFilterQueries(
+                "PROVIDER:\"The European Library\"",
+                "LANGUAGE:\"en\"",
+                "LANGUAGE:\"de\""
+        );
+        Map<String, String> facetMap = new HashMap<String, String>();
+        facetMap.put("PROVIDER", "prov");
+        facetMap.put("LANGUAGE", "lang");
+        String[] expectFilterQueries = new String[]{"{!tag=lang}LANGUAGE:(\"de\" OR \"en\")", "{!tag=prov}PROVIDER:\"The European Library\""};
+        log.info("expecting:");
+        for (String expect : expectFilterQueries) {
+            log.info(expect);
+        }
+        String[] actualFilterQueries = SolrQueryUtil.getFilterQueriesAsOrQueries(solrQuery, facetMap);
+        log.info("actual:");
+        for (String actual : actualFilterQueries) {
+            log.info(actual);
+        }
+        Assert.assertArrayEquals("arrays should be equal", expectFilterQueries, actualFilterQueries);
+    }
+   */
 }
