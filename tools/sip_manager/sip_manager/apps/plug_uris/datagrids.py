405de554f3b6a1c24094e75b09f504f5308171a0
jacob.lundqvist@gmail.com <jacob.lundqvist@gmail.com@3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8>
2010-04-20 14:50:23 +0000

    sipmanager restructuring
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@1743 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

---
+++ tools/sip_manager/sip_manager/apps/plug_uris/datagrids.py
@@ -0,0 +1,38 @@
+# myapp/datagrids.py
+from django.contrib.auth.models import User
+from djblets.datagrid.grids import Column, DataGrid
+
+import models
+
+COL_NAME = 'Name'
+COL_WAITING = 'Waiting'
+COL_OK = 'Ok'
+COL_BAD = 'Bad'
+
+class UserDataGrid(DataGrid):
+    idd = Column("id", sortable=True)
+    name = Column(COL_NAME, sortable=True)
+    waiting = Column(COL_WAITING, sortable=True)
+    ok = Column(COL_OK, sortable=True)
+    bad = Column(COL_BAD, sortable=True)
+
+    def __init__(self, request):
+        sel_common = "SELECT COUNT(*) FROM plug_uris_uri WHERE"
+        sql_table_join = "AND uri_source_id=plug_uris_urisource.id"
+
+        sql_img_ok = "%s status=%i %s" % (sel_common, models.URIS_COMPLETED, sql_table_join)
+        sql_img_waiting = "%s status=%i AND item_type=%i AND err_code=%i %s" % (
+            sel_common, models.URIS_CREATED, models.URIT_OBJECT, models.URIE_NO_ERROR, sql_table_join)
+        sql_img_bad = "%s err_code > %i %s" % (sel_common, models.URIE_NO_ERROR, sql_table_join)
+
+        sql = "SELECT task_eta FROM process_monitor_processmonitoring where pid=plug_uris_urisource.pid"
+        DataGrid.__init__(self, request,
+                          models.UriSource.objects.extra(select={
+                              'imgs_ok':sql_img_ok,
+                              'imgs_bad': sql_img_bad,
+                              'imgs_waiting':sql_img_waiting,
+                              #'eta':sql,
+                              }), "Uri sources")
+        #DataGrid.__init__(self, request, User.objects.filter(is_active=True), "Users")
+        self.default_sort = [COL_NAME]
+        self.default_columns = [COL_NAME, COL_WAITING, COL_OK, COL_BAD]
