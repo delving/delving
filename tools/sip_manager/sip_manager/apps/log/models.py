d91584a5141fae50866508255ab3113be0ce9a4a
jacob.lundqvist@gmail.com <jacob.lundqvist@gmail.com@3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8>
2010-04-20 14:50:01 +0000

    sipmanager restructuring
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@1742 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

---
+++ tools/sip_manager/sip_manager/apps/log/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from utils.gen_utils import dict_2_django_choice
+
+
+LOGE_NO_URIS = 1
+
+LOG_ERR_CODES = {
+    LOGE_NO_URIS : 'no uris',
+    }
+
+class ErrLog(models.Model):
+    err_code = models.IntegerField(choices=dict_2_django_choice(LOG_ERR_CODES))
+    msg = models.TextField()
+    item_id = models.CharField(max_length=200)
+    plugin_module = models.CharField(max_length=100)
+    plugin_name = models.CharField(max_length=100)
+    time_created = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        print '****Logged an error!!'
+        print '*\terr_code: [%i] %s' % (self.err_code, LOG_ERR_CODES[self.err_code])
+        print '*\tmsg:     ', self.msg
+        print '*\titem_id: ', self.item_id
+        print '*\tplugin:  ', self.plugin_name
+        super(ErrLog, self).save(*args, **kwargs)
