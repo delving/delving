501f7ba2c1734aa6980ef5997a6934b96510e8c2
serkan@blackbuilt.nl <serkan@blackbuilt.nl@3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8>
2010-04-21 13:55:28 +0000

    #970 loading existing snippets in Map<>
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@1762 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

--- sip-creator/src/main/java/eu/europeana/sip/io/GroovyMappingFileImpl.java
+++ sip-creator/src/main/java/eu/europeana/sip/io/GroovyMappingFileImpl.java
@@ -4,11 +4,12 @@ import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.LineNumberReader;
+import java.io.RandomAccessFile;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Implementation of GroovyMappingFile // todo: retrieve from FileSet
@@ -38,29 +39,18 @@ public class GroovyMappingFileImpl implements GroovyMappingFile {
         }
     }
 
-    private void load() throws IOException { // todo: broken, skips delimiters
-        LineNumberReader lineNumberReader = new LineNumberReader(new FileReader(file));
-        String line;
-        String delimiter;
-        StringBuffer snippet = new StringBuffer();
-        while (null != (line = lineNumberReader.readLine())) {
-            if (line.contains(NEXT_NODE_IDENTIFIER)) {
-                LOG.info(String.format("Found ID %s%n", line));
-                //snippet.append(String.format("%s%n", line));
-                delimiter = line;
-                while (null != (line = lineNumberReader.readLine())) {
-                    if (line.contains(NEXT_NODE_IDENTIFIER) || line.contains("EOF")) {
-                        LOG.info(String.format("Next node or EOF found, break%n"));
-                        break;
-                    }
-                    LOG.info(String.format("Appending to snippet %s%n", snippet));
-                    LOG.info(String.format("Line : %s%n", line));
-                    snippet.append(String.format("%s%n", line));
-                }
-                snippets.put(new Delimiter(delimiter), snippet.toString());
-            }
+    // todo: not working properly; regenerating groovyLoops from scratch based on delimiter
+    // todo: also loading entire file to buffer
+
+    private void load() throws IOException {
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r");
+        byte[] buffer = new byte[(int) randomAccessFile.length()];
+        randomAccessFile.readFully(buffer);
+        Pattern pattern = Pattern.compile("(//\\s===\\sMAPPING\\((.+)\\)\\s===)");
+        Matcher matcher = pattern.matcher(new String(buffer));
+        while (matcher.find()) {
+            snippets.put(new Delimiter(matcher.group(2)), GroovyService.generateGroovyLoop(matcher.group(2)));
         }
-        lineNumberReader.close();
         LOG.info(String.format("Loaded %d snippets%n", snippets.size()));
     }
 
