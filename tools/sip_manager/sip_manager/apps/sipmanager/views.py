0b5f0c5b8f25553035d065cd6a83368106b3531d
jacob.lundqvist@gmail.com <jacob.lundqvist@gmail.com@3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8>
2010-04-27 11:18:33 +0000

    sipmanager added monitor page and top index
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@1859 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

--- tools/sip_manager/sip_manager/apps/sipmanager/views.py
+++ tools/sip_manager/sip_manager/apps/sipmanager/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+
+from django.shortcuts import render_to_response, get_object_or_404
+
+
+import models
+
+
+
+
+def show_monitor(request):
+    procs = models.ProcessMonitoring.objects.order_by('time_created')
+    return render_to_response("sipmanager/show_procs.html",
+                              {
+                                  'request': request,
+                                  'procs': procs,
+                              })
