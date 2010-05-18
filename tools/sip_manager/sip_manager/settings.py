65fa3af25f5baed95bcc899b365e4ab12c802fa3
jacob.lundqvist@gmail.com <jacob.lundqvist@gmail.com@3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8>
2010-05-18 14:17:48 +0000

    sipmanager completed rescedule functionality for failed images
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@2045 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

--- tools/sip_manager/sip_manager/settings.py
+++ tools/sip_manager/sip_manager/settings.py
@@ -238,6 +238,7 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.csrf.middleware.CsrfMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
