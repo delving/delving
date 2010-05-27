3453a356395da055e9f878047f3f9f16d7c51b60
geralddejong@gmail.com <geralddejong@gmail.com@3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8>
2010-05-27 17:25:20 +0000

    #1095 Repaired unit test
    git-svn-id: https://www.europeanalabs.eu/svn/europeana/trunk@2160 3c7f12e8-d31e-4037-9ce9-7a8ea9e2c7c8

--- sip-creator/src/test/java/eu/europeana/sip/model/TestRecordValidator.java
+++ sip-creator/src/test/java/eu/europeana/sip/model/TestRecordValidator.java
@@ -38,14 +38,14 @@ import static junit.framework.Assert.assertEquals;
 
 public class TestRecordValidator {
     private static final String[] VALID_FIELDS = {
-            "<europeana:country>country</europeana:country>",
-            "<europeana:europeanaCollectionName>collectionName</europeana:europeanaCollectionName>",
+            "<europeana:country>NETHERLANDS</europeana:country>",
+            "<europeana:collectionName>collectionName</europeana:collectionName>",
             "<europeana:isShownAt>http://is-shown-at.com/</europeana:isShownAt>",
             "<europeana:isShownBy>http://is-shown-by.com/</europeana:isShownBy>",
-            "<europeana:language>language</europeana:language>",
+            "<europeana:language>EN</europeana:language>",
             "<europeana:object>http://object.com/</europeana:object>",
             "<europeana:provider>provider</europeana:provider>",
-            "<europeana:type>type</europeana:type>",
+            "<europeana:type>IMAGE</europeana:type>",
             "<europeana:uri>http://uri.com/</europeana:uri>",
     };
     private RecordValidator recordValidator;
