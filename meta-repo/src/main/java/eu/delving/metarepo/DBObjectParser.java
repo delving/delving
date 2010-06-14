1f3d6f48a5b9ed260310b79b2be8c5f5d77f59f8
Gerald de Jong <geralddejong@gmail.com>
2010-06-14 13:57:19 +0200

    parser producing db objects

--- meta-repo/src/main/java/eu/delving/metarepo/DBObjectParser.java
+++ meta-repo/src/main/java/eu/delving/metarepo/DBObjectParser.java
@@ -24,95 +24,86 @@ public class DBObjectParser {
     private InputStream inputStream;
     private XMLStreamReader2 input;
     private QName recordRoot;
-    private int recordCount;
-    private Listener listener;
+    private String metadataFormat;
 
-    public interface Listener {
-        void recordsParsed(int count, boolean lastRecord);
-    }
-
-    public DBObjectParser(InputStream inputStream, QName recordRoot, Listener listener) throws XMLStreamException {
+    public DBObjectParser(InputStream inputStream, QName recordRoot, String metadataFormat) throws XMLStreamException {
         this.inputStream = inputStream;
         this.recordRoot = recordRoot;
-        this.listener = listener;
+        this.metadataFormat = metadataFormat;
         XMLInputFactory2 xmlif = (XMLInputFactory2) XMLInputFactory2.newInstance();
         xmlif.setProperty(XMLInputFactory.IS_REPLACING_ENTITY_REFERENCES, Boolean.FALSE);
         xmlif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, Boolean.FALSE);
         xmlif.setProperty(XMLInputFactory.IS_COALESCING, Boolean.FALSE);
         xmlif.configureForSpeed();
-        this.input = (XMLStreamReader2) xmlif.createXMLStreamReader("Normalization", inputStream);
+        this.input = (XMLStreamReader2) xmlif.createXMLStreamReader(getClass().getName(), inputStream);
     }
 
     @SuppressWarnings("unchecked")
     public synchronized DBObject nextRecord() throws XMLStreamException, IOException {
         DBObject object = null;
-        DBObject rootNode = null;
-        Stack<DBObject> nodeStack = new Stack<DBObject>();
-        StringBuilder value = new StringBuilder();
+        StringBuilder recordContent = new StringBuilder();
         boolean withinRecord = false;
+        int depth = 0;
+        int recordDepth = 0;
+        boolean contentPresent = false;
         while (object == null) {
             switch (input.getEventType()) {
                 case XMLEvent.START_DOCUMENT:
-                    if (listener != null) {
-                        listener.recordsParsed(0, false);
-                    }
+                    break;
+                case XMLEvent.NAMESPACE:
                     break;
                 case XMLEvent.START_ELEMENT:
-                    if (input.getName().equals(recordRoot)) {
-                        withinRecord = true;
+                    depth++;
+                    if (!withinRecord) {
+                        if (input.getName().equals(recordRoot)) {
+                            withinRecord = true;
+                            recordDepth = depth;
+                        }
                     }
-                    if (withinRecord) {
-                        DBObject parent;
-                        if (nodeStack.isEmpty()) {
-                            parent = null;
+                    else {
+                        if (contentPresent) {
+                            throw new IOException("Content and subtags not permitted");
                         }
-                        else {
-                            parent = nodeStack.peek();
-                        }
-                        String nodeName;
-                        if (null == input.getPrefix()) {
-                            nodeName = input.getName().equals(recordRoot) ? "input" : input.getLocalName();
-                        }
-                        else {
-                            nodeName = input.getName().equals(recordRoot) ? "input" : input.getPrefix() + "_" + input.getLocalName();
-                        }
-                        DBObject node = new BasicDBList(); // todo: used to use parent, nodeName
+                        recordContent.append("<").append(input.getPrefixedName());
                         if (input.getAttributeCount() > 0) {
                             for (int walk = 0; walk < input.getAttributeCount(); walk++) {
-                                QName attributeName = input.getAttributeName(walk);
-//                                node.attributes().put(attributeName.getLocalPart(), input.getAttributeValue(walk)); todo
+                                QName qName = input.getAttributeName(walk);
+                                String attrName = qName.getPrefix().isEmpty() ? qName.getLocalPart() : qName.getPrefix() + ":" + qName.getLocalPart();
+                                String value = input.getAttributeValue(walk);
+                                recordContent.append(' ').append(attrName).append("=\"").append(value).append("\"");
                             }
                         }
-                        nodeStack.push(node);
-                        if (parent == null) {
-                            rootNode = node;
-                        }
-                        value.setLength(0);
+                        recordContent.append(">\n");
                     }
                     break;
                 case XMLEvent.CHARACTERS:
                 case XMLEvent.CDATA:
                     if (withinRecord) {
-                        value.append(input.getText());
+                        if (!contentPresent && !input.getText().trim().isEmpty()) {
+                            contentPresent = true;
+                            recordContent.append(input.getText());
+                        }
                     }
                     break;
                 case XMLEvent.END_ELEMENT:
-                    if (input.getName().equals(recordRoot)) {
-                        withinRecord = false;
-                        recordCount++;
-                        object = new BasicDBObject();
-                        if (listener != null) {
-                            listener.recordsParsed(recordCount, false);
-                        }
-                    }
                     if (withinRecord) {
-                        DBObject node = nodeStack.pop();
-                        String valueString = value.toString().replaceAll("\n", " ").replaceAll(" +", " ").trim();
-                        value.setLength(0);
-                        if (valueString.length() > 0) {
-//                            node.setValue(valueString); todo
+                        if (input.getName().equals(recordRoot) && depth == recordDepth) {
+                            withinRecord = false;
+                            // todo: unique!
+                            // todo: lastModified
+                            object = new BasicDBObject();
+                            object.put(metadataFormat, recordContent.toString());
+                            recordContent.setLength(0);
+                        }
+                        else {
+                            if (contentPresent) {
+                                recordContent.append('\n');
+                            }
+                            recordContent.append("</").append(input.getPrefixedName()).append(">\n");
+                            contentPresent = false;
                         }
                     }
+                    depth--;
                     break;
                 case XMLEvent.END_DOCUMENT: {
                     break;
@@ -120,9 +111,6 @@ public class DBObjectParser {
             }
             if (!input.hasNext()) {
                 inputStream.close();
-                if (listener != null) {
-                    listener.recordsParsed(recordCount, true);
-                }
                 break;
             }
             input.next();
