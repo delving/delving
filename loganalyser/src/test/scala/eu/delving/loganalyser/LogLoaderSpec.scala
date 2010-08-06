d829cfd87f15ed124f9fbb4ce71a5a427730fc8f
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2010-08-06 22:29:50 +0200

    added sample logfiles for test purposes

---
+++ loganalyser/src/test/scala/eu/delving/loganalyser/LogLoaderSpec.scala
@@ -0,0 +1,14 @@
+package eu.delving.loganalyser
+
+import org.scalatest.matchers.ShouldMatchers
+import tools.cmd.Spec
+
+/**
+ *
+ * @author Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
+ * @since Aug 6, 2010 10:14:52 PM
+ */
+
+class LogLoaderSpec extends Spec with ShouldMatchers {
+  
+}
