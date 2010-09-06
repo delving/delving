2ffefc07c9411dc7e9f20029b0ac68e033e8af61
Gerald de Jong <gerald@delving.eu>
2010-09-06 10:15:23 +0200

    added ROLE_RESEARCH_USER

--- core/src/main/java/eu/europeana/core/database/domain/Role.java
+++ core/src/main/java/eu/europeana/core/database/domain/Role.java
@@ -6,6 +6,7 @@ package eu.europeana.core.database.domain;
 
 public enum Role {
     ROLE_USER,  // general portal user, no dashboard access
+    ROLE_RESEARCH_USER, // to be used for "museometrie"
     ROLE_CONTENT_TESTER, // a user of the special sandbox version of the dashboard
     ROLE_TRANSLATOR, // translation tab only
     ROLE_EDITOR, //  carousel + proposed search terms
