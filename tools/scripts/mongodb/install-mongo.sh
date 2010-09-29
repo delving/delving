7527ff8bdbfeabc031d1c60e748ddc7bbdd883fe
Gerald de Jong <gerald@delving.eu>
2010-09-29 12:30:27 +0200

    Scripts for running mongodb as daemon on OSX

---
+++ tools/scripts/mongodb/install-mongo.sh
@@ -0,0 +1,28 @@
+
+#!/usr/bin/env bash
+
+# MongoDB OSX Launchctl Item
+# Author: Andrei Railean (http://andrei.md)
+if [[ `whoami` != "root" ]]; then
+echo "This script must be run as root; use sudo"
+  exit
+fi
+
+echo "Installing MongoDB Launchctl Item and Restarting Mongo";
+
+# create directories
+sudo mkdir -p /opt/local/var/db/mongodb
+sudo mkdir -p /opt/local/var/log/
+sudo touch /opt/local/var/log/mongodb.log
+
+# copy PLIST and (re)install it
+sudo cp org.mongo.mongod.plist /System/Library/LaunchDaemons/.
+sudo chown root:wheel /System/Library/LaunchDaemons/org.mongo.mongod.plist
+sudo launchctl stop org.mongo.mongod
+sudo launchctl unload /System/Library/LaunchDaemons/org.mongo.mongod.plist
+sudo launchctl load /System/Library/LaunchDaemons/org.mongo.mongod.plist
+sudo launchctl start org.mongo.mongod
+
+echo "FINISHED";
+
+
