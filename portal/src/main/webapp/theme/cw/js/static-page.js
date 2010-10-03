d2283c73bbfe4ef128384e95608d60b9cda15ea8
zemyatin <eric@delving.eu>
2010-10-03 19:37:58 +0200

    html editor now has button (1st in the toolbar) where a user can select a template containing prefab html snippets. Two templates have been added along with a new directory under Theme called "snippets"

---
+++ portal/src/main/webapp/theme/cw/js/static-page.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    tinyMCE.init({
+        mode : "textareas",
+        theme : "advanced",
+        plugins: "advimage,template",
+        body_class : "${portalColor}",
+        relative_urls : false,
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_buttons1 : "|,template,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,|,bullist,numlist,|,undo,redo,|,link,unlink,anchor,|,image,|,forecolor,backcolor,|,removeformat,code",
+        theme_advanced_buttons2 : "",
+        theme_advanced_statusbar_location : "bottom",
+        content_css : baseThemePath+"/css/reset-text-grid.css,"+baseThemePath+"/css/type.css,"+baseThemePath+"/css/screen.css,"+baseThemePath+"/css/colors.css",
+        external_image_list_url : "/${portalName}/_.img?javascript=true",
+        template_external_list_url : baseThemePath+"/js/tiny-templates.js"
+    })
+
+});
+function toggleEditor(id) {
+    if (!tinyMCE.get(id)) {
+        tinyMCE.execCommand('mceAddControl', false, id);
+    } else {
+        tinyMCE.execCommand('mceRemoveControl', false, id);
+    }
+}
---
+++ portal/src/main/webapp/theme/cw/js/tiny-templates.js
@@ -0,0 +1,9 @@
+// This list may be created by a server logic page PHP/ASP/ASPX/JSP in some backend system.
+// There templates will be displayed as a dropdown in all media dialog if the "template_external_list_url"
+// option is defined in TinyMCE init.
+
+var tinyMCETemplateList = [
+	// Name, URL, Description
+	["Informatie \"box\" 1", "/portal/theme/cw/snippets/cw-box-open.template", "Informatie \"box\" met titel"],
+    ["Informatie \"box\" 2", "/portal/theme/cw/snippets/cw-box-closed.template", "Informatie \"box\" met titel en achtergrondkleur"]    
+];
