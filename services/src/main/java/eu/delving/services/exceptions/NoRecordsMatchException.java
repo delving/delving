0345c0702d69a4b0a46abda1003b7c3498062aaa
Gerald de Jong <gerald@delving.eu>
2010-10-11 14:08:13 +0200

    Exceptions should delegate the message up the hierarchy

--- services/src/main/java/eu/delving/services/exceptions/NoRecordsMatchException.java
+++ services/src/main/java/eu/delving/services/exceptions/NoRecordsMatchException.java
@@ -4,16 +4,9 @@ package eu.delving.services.exceptions;
  * @author Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
  * @since Jun 27, 2010 5:19:26 PM
  */
+
 public class NoRecordsMatchException extends Exception {
-    private static final long serialVersionUID = 942631363533503772L;
-
-    private String errorMessage;
-
     public NoRecordsMatchException(String errorMessage) {
-        this.errorMessage = errorMessage;
-    }
-
-    public String getErrorMessage() {
-        return errorMessage;
+        super(errorMessage);
     }
 }
