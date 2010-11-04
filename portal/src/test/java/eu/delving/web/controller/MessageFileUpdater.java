00c38e43d2c7b090d44ca8e3959e9822335c958f
Gerald de Jong <gerald@delving.eu>
2010-11-04 13:19:47 +0100

    MessageFileUpdater set up and messages files all cleaned and sorted

--- portal/src/test/java/eu/delving/web/controller/MessageFileUpdater.java
+++ portal/src/test/java/eu/delving/web/controller/MessageFileUpdater.java
@@ -2,15 +2,8 @@ package eu.delving.web.controller;
 
 import com.mongodb.Mongo;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Map;
-import java.util.TreeMap;
 
 /**
  * Fetch changes from mongo for message files
@@ -19,41 +12,6 @@ import java.util.TreeMap;
  */
 
 public class MessageFileUpdater {
-
-    private static Map<String, String> readMap(File file) throws IOException {
-        Map<String, String> map = new TreeMap<String, String>();
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
-        String line;
-        while ((line = br.readLine()) != null) {
-            line = line.trim();
-            if (line.startsWith("#") || line.isEmpty()) {
-                continue;
-            }
-            int equals = line.indexOf("=");
-            if (equals < 0) {
-                throw new IOException("No equals sign found: " + line);
-            }
-            String key = line.substring(0, equals).trim();
-            StringBuilder value = new StringBuilder();
-            String valuePart = line.substring(equals + 1).trim();
-            while (valuePart.endsWith("\\")) {
-                value.append(valuePart.substring(0, valuePart.length() - 1));
-                value.append(' ');
-                valuePart = br.readLine().trim();
-            }
-            value.append(valuePart);
-            map.put(key, value.toString().trim());
-        }
-        return map;
-    }
-
-    private static void writeMap(Map<String, String> map, File file) throws IOException {
-        OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            out.write(String.format("%s=%s\n", entry.getKey(), entry.getValue()));
-        }
-        out.close();
-    }
 
     public static void main(String[] args) throws Exception {
         if (args.length < 2) {
@@ -77,7 +35,7 @@ public class MessageFileUpdater {
             if (!messageFile.exists()) {
                 throw new Exception("File doesn't exist! " + messageFile.getAbsolutePath());
             }
-            Map<String,String> existing = readMap(messageFile);
+            Map<String,String> existing = MessageFileUtil.readMap(messageFile);
             Map<String, String> changes = messageFileEntry.getValue();
             for (Map.Entry<String, String> entry : changes.entrySet()) {
                 System.out.println(String.format(
@@ -88,7 +46,7 @@ public class MessageFileUpdater {
                 ));
                 existing.put(entry.getKey(), entry.getValue());
             }
-            writeMap(existing, messageFile);
+            MessageFileUtil.writeMap(existing, messageFile);
         }
     }
 }
