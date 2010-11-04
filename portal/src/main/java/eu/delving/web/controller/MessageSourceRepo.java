ffcee11514db252ecd13846593e0c60ab4219410
Gerald de Jong <gerald@delving.eu>
2010-11-04 12:24:01 +0100

    MessageSourceRepo unit tested, and produces maps of changes that MessageFileUpdater can use to update message files

--- portal/src/main/java/eu/delving/web/controller/MessageSourceRepo.java
+++ portal/src/main/java/eu/delving/web/controller/MessageSourceRepo.java
@@ -13,8 +13,8 @@ import org.springframework.context.support.AbstractMessageSource;
 
 import java.text.MessageFormat;
 import java.util.Locale;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * The repository of i18n messages
@@ -31,22 +31,15 @@ public class MessageSourceRepo extends AbstractMessageSource {
     @Autowired
     private Mongo mongo;
 
+    public void setMongo(Mongo mongo) {
+        this.mongo = mongo;
+    }
+
     @Value("#{launchProperties['portal.mongo.dbName']}")
     private String databaseName;
 
     public void setDatabaseName(String databaseName) {
         this.databaseName = databaseName;
-    }
-
-    public Set<String> getMessageKeys() {
-        messages().ensureIndex(new BasicDBObject(KEY, 1));
-        DBCursor cursor = messages().find();
-        Set<String> set = new TreeSet<String>();
-        while (cursor.hasNext()) {
-            DBObject pageObject = cursor.next();
-            set.add((String) pageObject.get(KEY));
-        }
-        return set;
     }
 
     public Translation getTranslation(String key) {
@@ -61,6 +54,27 @@ public class MessageSourceRepo extends AbstractMessageSource {
 
     public void setTranslation(String key, String message, Locale locale) {
         getTranslation(key).setContent(message, locale).save();
+    }
+
+    public Map<String, Map<String, String>> getMessageFileMaps() {
+        Map<String, Map<String, String>> fileMap = new TreeMap<String,Map<String,String>>();
+        DBCursor cursor = messages().find();
+        while (cursor.hasNext()) {
+            DBObject object = cursor.next();
+            String messsageKey = (String) object.get(KEY);
+            for (String contentKey : object.keySet()) {
+                if (contentKey.startsWith(CONTENT)) {
+                    String fileName = String.format("messages%s.properties",contentKey.substring(CONTENT.length()));
+                    Map<String, String> map = fileMap.get(fileName);
+                    if (map == null) {
+                        map = new TreeMap<String, String>();
+                        fileMap.put(fileName, map);
+                    }
+                    map.put(messsageKey, (String)object.get(contentKey));
+                }
+            }
+        }
+        return fileMap;
     }
 
     @Override
@@ -92,7 +106,6 @@ public class MessageSourceRepo extends AbstractMessageSource {
             String content = (String) object.get(objectKey(locale));
             if (content == null) {
                 content = getParentMessageSource().getMessage(getKey(), null, locale);
-//                setContent(content, locale).save();
             }
             return content;
         }
