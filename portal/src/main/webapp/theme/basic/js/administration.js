5b91c7dffcd0548798d4243c9b343ede4ae4afd0
zemyatin <eric@delving.eu>
2010-12-04 18:47:14 +0100

    major theme changes

---
+++ portal/src/main/webapp/theme/basic/js/administration.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+     $.ajax({
+        url: portalName+"/users.ajax",
+        type: "GET",
+         dataType: "xml",
+        success: function(xml) {
+            var openTable = "<table id='user-table' class='tablesorter'>";
+            var closeTable = "</tbody></table>"
+            var userDataRow = "<thead><tr><th>Emailadres</th><th>Huidige Rol</th><th>Voornaam</th><th>Achternaam</th>";
+            userDataRow += "<th>Gebruikersnaam</th><th>Registratiedatum</th><th>Laatste inlogdatum</th></thead><tbody>";
+            var userRoleSelect = '<select name="newRole">';
+            userRoleSelect += '<option>Kies een rol</option>';
+
+            $(xml).find('user').each(function(){
+                var _email = $(this).attr('email');
+                var _role = $(this).attr('role');
+                var _firstName = $(this).attr('firstName');
+                var _lastName = $(this).attr('lastName');
+                var _userName = $(this).attr('userName');
+                var _regDate = $(this).attr('registrationDate');
+                var _lastLoginDate = $(this).attr('lastLoginDate');
+                userDataRow += '<tr><td class="searchParam"><a href="#" class="searchParam"><span class="ui-icon ui-icon-person"></span>'+_email+'</a></td><td>'+_role+'</td><td>'+_firstName+'</td><td>'+_lastName+'</td>';
+                userDataRow += '<td>'+_userName+'<td>'+_regDate+'</td><td>'+_lastLoginDate+'</td></tr>';
+            });
+            $('div#all-users-list').html("<h2>Gebruikers lijst</h2>"+openTable+userDataRow+closeTable);
+            $("#user-table").tablesorter({
+                widgets: ['zebra']});
+            styleUIButtons();
+            $('td.searchParam, a.searchParam').click(function(){
+                $('input#searchPattern').val($(this).text());
+                $('form#search-form"').submit();
+                return false;
+            })
+        },
+        error: function(xml) {
+            alert(xml);
+            showMessage("fail","Something went wrong");
+        }
+    });
+    $('button#rem-user').click(function(){
+        var toRemove = $('input#userEmail').val();
+        var confirmation = confirm("Gebruiker: "+toRemove +" verwijderen ?");
+        if(confirmation){
+            $.ajax({
+              type: 'POST',
+              url: portalName+'/user-remove.ajax',
+              data: "email="+toRemove,
+              success: function(data){
+                  window.location.href=window.location.href;
+              }
+            });
+            return false;
+        } else {
+            return false;
+        }
+    })
+})
--- portal/src/main/webapp/theme/basic/js/js_utilities.js
+++ portal/src/main/webapp/theme/basic/js/js_utilities.js
@@ -79,12 +79,34 @@ function delvingPageCall(targetId,pageName,msgHead,msgBody,msgLink){
     });
 }
 
+function showMessage(messageClass, messageString){
+    $("#messages .message").html(messageString);
+    $("#messages").addClass(messageClass).slideDown("slow").click(function(){
+        $(this).slideUp("slow").delay(2000).css("display","none");
+    });
 
+}
 
-$(document).ready(function() {
+function styleUIButtons(){
+
+    // style all the submit and button elements.
 
     var buttons = $(document).find("input[type=submit],input[type=reset],button,a.button");
         buttons.addClass("fg-button ui-state-default ui-corner-all");
+        buttons.css({'padding':'0.2em .25em'});
+// Todo: when icons added FF does not render them in the desired position
+        buttons
+            .filter(".btn-strong")
+            .addClass("fg-button-icon-right")
+            .css({"background":"#01689b","border":"1px solid #000000","color":"#ffffff"});
+        buttons
+            .filter(".adm")
+            .addClass("fg-button-icon-left")
+            .css({"padding":"4px 4px 4px 20px"});
+        buttons
+            .filter(".delete")
+            .css({"background":"firebrick"});
+    //    ui button hover states
 	$(function(){
 		//all hover and click logic for buttons
 		$(".fg-button:not(.ui-state-disabled)")
@@ -107,12 +129,34 @@ $(document).ready(function() {
 			}
 		});
 	});
+}
 
+function styleUIMessages(){
+    var infoboxes = $(document).find("div.ui-info");
+
+        if(infoboxes){
+            infoboxes.addClass("ui-state-highlight ui-corner-all");
+            infoboxes.css({'margin-top':'20px','padding':'0pt 0.7em'});
+            infoboxes.append('<p><span class="ui-icon ui-icon-info" style="float: left; margin-right: 0.3em;"></span><span class="message">'+$(this).html()+'</span></p>');
+        }
+
+        var errorboxes = $(document).find("div.ui-error");
+
+        if(errorboxes){
+            errorboxes.addClass("ui-state-error ui-corner-all");
+            errorboxes.css({'margin-top':'20px','padding':'0pt 0.7em'});
+            errorboxes.append('<p><span class="ui-icon ui-icon-alert" style="float: left; margin-right: 0.3em;"></span><span class="message">Test</span></p>');
+        }
+}
+
+
+$(document).ready(function() {
+
+   styleUIButtons();
     //onclick for login href to take user back to last visited page before logging in
     if($("a#login")){
         $("a#login").click(function(){
             takeMeBack();
-            //alert('foo');
             $("div#overlayContainer").dialog({
                 modal: true,
                 width: 350
@@ -120,6 +164,11 @@ $(document).ready(function() {
             $("div#overlayContainer").load("login.html?contentOnly=true");
             
             return false;
+        })
+    }
+    if($("a#logout")){
+        $("a#logout").click(function(){
+            takeMeBack();
         })
     }
 });
