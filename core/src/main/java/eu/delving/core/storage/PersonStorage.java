aff57a6c0b9312aa56b544494eb0f4fdc04a6ed7
Gerald de Jong <gerald@delving.eu>
2010-12-18 20:28:31 +0100

    First hack at PersonStorage to replace UserDao and use MongoDB

---
+++ core/src/main/java/eu/delving/core/storage/PersonStorage.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2010 DELVING BV
+ *
+ *  Licensed under the EUPL, Version 1.0 or? as soon they
+ *  will be approved by the European Commission - subsequent
+ *  versions of the EUPL (the "Licence");
+ *  you may not use this work except in compliance with the
+ *  Licence.
+ *  You may obtain a copy of the Licence at:
+ *
+ *  http://ec.europa.eu/idabc/eupl
+ *
+ *  Unless required by applicable law or agreed to in
+ *  writing, software distributed under the Licence is
+ *  distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied.
+ *  See the Licence for the specific language governing
+ *  permissions and limitations under the Licence.
+ */
+
+package eu.delving.core.storage;
+
+import eu.delving.domain.Language;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Gerald de Jong <gerald@delving.eu>
+ */
+
+public interface PersonStorage {
+
+    Person authenticate(String email, String password);
+    List<Person> getPeople(String pattern);
+    List<Person> getPeople();
+    Person byEmail(String email);
+
+    String PERSON_DATABASE = "persons";
+
+    public interface Person {
+        void setEmail(String email);
+        String getEmail();
+        void setEnabled(boolean enabled);
+        boolean isEnabled();
+        void setFirstName(String firstName);
+        String getFirstName();
+        void setLastName(String lastName);
+        String getLastName();
+        void setLastLogin(Date lastLogin);
+        Date getLastLogin();
+        Item addItem(String author, String title, Language language);
+        List<Item> getItems();
+        Search addSearch(String query, String queryString, Language language);
+        List<Search> getSearches();
+        void save();
+        void delete();
+
+        String EMAIL = "email";
+        String PASSWORD = "password";
+        String ENABLED = "enabled";
+        String FIRST_NAME = "first_name";
+        String LAST_NAME = "last_name";
+        String LAST_LOGIN = "last_login";
+        String ITEMS = "items";
+        String SEARCHES = "searches";
+    }
+
+    public interface Item {
+        String getAuthor();
+        String getTitle();
+        Language getLanguage();
+        // todo: identify the actual object (was europeanaId
+        Date getDateSaved();
+        void remove();
+
+        String AUTHOR = "author";
+        String TITLE = "title";
+        String LANGUAGE = "lang";
+        String DATE_SAVED = "date_saved";
+    }
+
+    public interface Search {
+        String getQuery();
+        String getQueryString();
+        Language getLanguage();
+        Date getDateSaved();
+        void remove();
+
+        String QUERY = "query";
+        String QUERY_STRING = "query_string";
+        String LANGUAGE = "lang";
+        String DATE_SAVED = "date_saved";
+    }
+}
---
+++ core/src/main/java/eu/delving/core/storage/PersonStorageImpl.java
@@ -0,0 +1,271 @@
+/*
+ * Copyright 2010 DELVING BV
+ *
+ *  Licensed under the EUPL, Version 1.0 or? as soon they
+ *  will be approved by the European Commission - subsequent
+ *  versions of the EUPL (the "Licence");
+ *  you may not use this work except in compliance with the
+ *  Licence.
+ *  You may obtain a copy of the Licence at:
+ *
+ *  http://ec.europa.eu/idabc/eupl
+ *
+ *  Unless required by applicable law or agreed to in
+ *  writing, software distributed under the Licence is
+ *  distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied.
+ *  See the Licence for the specific language governing
+ *  permissions and limitations under the Licence.
+ */
+
+package eu.delving.core.storage;
+
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import eu.delving.domain.Language;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Gerald de Jong <gerald@delving.eu>
+ */
+
+public class PersonStorageImpl implements PersonStorage {
+
+    @Autowired
+    private Mongo mongo;
+
+    private DBCollection persons() {
+        return mongo.getDB(PERSON_DATABASE).getCollection(PERSON_DATABASE);
+    }
+
+    public Person authenticate(String email, String password) {
+        persons().ensureIndex(new BasicDBObject(Person.EMAIL, 1));
+        DBObject query = new BasicDBObject();
+        query.put(Person.EMAIL, email);
+        query.put(Person.PASSWORD, password);
+        DBObject personObject = persons().findOne(query);
+        return personObject != null ? new PersonImpl(personObject) : null;
+    }
+
+    public List<Person> getPeople(String pattern) {
+        return null;
+    }
+
+    public List<Person> getPeople() {
+        List<Person> persons = new ArrayList<Person>();
+        for (DBObject personObject : persons().find()) {
+            persons.add(new PersonImpl(personObject));
+        }
+        return persons;
+    }
+
+    public Person byEmail(String email) {
+        persons().ensureIndex(new BasicDBObject(Person.EMAIL, 1));
+        DBObject query = new BasicDBObject();
+        query.put(Person.EMAIL, email);
+        DBObject personObject = persons().findOne(query);
+        return personObject != null ? new PersonImpl(personObject) : null;
+    }
+
+    public class PersonImpl implements Person {
+        private DBObject object;
+
+        public PersonImpl(DBObject object) {
+            this.object = object;
+        }
+
+        public void setEmail(String email) {
+            object.put(EMAIL, email);
+        }
+
+        public String getEmail() {
+            return (String) object.get(EMAIL);
+        }
+
+        public void setEnabled(boolean enabled) {
+            object.put(ENABLED, enabled);
+        }
+
+        public boolean isEnabled() {
+            return (Boolean) object.get(ENABLED);
+        }
+
+        public void setFirstName(String firstName) {
+            object.put(FIRST_NAME, firstName);
+        }
+
+        public String getFirstName() {
+            return (String) object.get(FIRST_NAME);
+        }
+
+        public void setLastName(String lastName) {
+            object.put(LAST_LOGIN, lastName);
+        }
+
+        public String getLastName() {
+            return (String) object.get(LAST_NAME);
+        }
+
+        public void setLastLogin(Date lastLogin) {
+            object.put(LAST_LOGIN, lastLogin);
+        }
+
+        public Date getLastLogin() {
+            return (Date) object.get(LAST_LOGIN);
+        }
+
+        public Item addItem(String author, String title, Language language) {
+            ItemImpl item = new ItemImpl(this);
+            item.setAuthor(author);
+            item.setTitle(title);
+            item.setLanguage(language);
+            return item;
+        }
+
+        public List<Item> getItems() {
+            List<Item> items = new ArrayList<Item>();
+            for (Object element : (BasicDBList) object.get(Person.ITEMS)) {
+                items.add(new ItemImpl(this, (DBObject)element));
+            }
+            return items;             
+        }
+        
+        public Search addSearch(String query, String queryString, Language language) {
+            SearchImpl search = new SearchImpl(this);
+            search.setQuery(query);
+            search.setQueryString(queryString);
+            search.setLanguage(language);
+            return search;
+        }
+        
+        public List<Search> getSearches() {
+            List<Search> searches = new ArrayList<Search>();
+            for (Object element : (BasicDBList) object.get(Person.SEARCHES)) {
+                searches.add(new SearchImpl(this, (DBObject)element));
+            }
+            return searches;             
+        }
+        
+        public void save() {
+            persons().save(object);
+        }
+        
+        public void delete() {
+            persons().remove(object);
+        }
+    }
+
+    public class ItemImpl implements Item {
+        private PersonImpl person;
+        private DBObject object;
+
+        public ItemImpl(PersonImpl person, DBObject object) {
+            this.person = person;
+            this.object = object;
+        }
+
+        public ItemImpl(PersonImpl person) {
+            this(person, new BasicDBObject(DATE_SAVED, new Date()));
+        }
+
+        public void setAuthor(String author) {
+            object.put(AUTHOR, author);
+        }
+
+        public String getAuthor() {
+            return (String) object.get(AUTHOR);
+        }
+
+        public void setTitle(String title) {
+            object.put(TITLE, title);
+        }
+
+        public String getTitle() {
+            return (String) object.get(TITLE);
+        }
+
+        public void setLanguage(Language language) {
+            object.put(LANGUAGE, language.toString());
+        }
+
+        public Language getLanguage() {
+            return Language.valueOf((String)object.get(LANGUAGE));
+        }
+
+        // todo: identify the actual object (was europeanaId)
+
+        public Date getDateSaved() {
+            return (Date) object.get(DATE_SAVED);
+        }
+
+        public void add() {
+            BasicDBList list = (BasicDBList) person.object.get(Person.ITEMS);
+            list.add(object);
+        }
+
+        public void remove() {
+            BasicDBList list = (BasicDBList) person.object.get(Person.ITEMS);
+            list.remove(object); // todo: will this work?
+        }
+    }
+
+    public class SearchImpl implements Search {
+        private PersonImpl person;
+        private DBObject object;
+
+        public SearchImpl(PersonImpl person, DBObject object) {
+            this.person = person;
+            this.object = object;
+        }
+
+        public SearchImpl(PersonImpl person) {
+            this(person, new BasicDBObject(DATE_SAVED, new Date()));
+        }
+
+        public void setQuery(String query) {
+            object.put(QUERY, query);
+        }
+
+        public String getQuery() {
+            return (String) object.get(QUERY);
+        }
+        
+        public void setQueryString(String queryString) {
+            object.put(QUERY_STRING, queryString);
+        }
+
+        public String getQueryString() {
+            return (String) object.get(QUERY_STRING);
+        }
+        
+        public void setLanguage(Language language) {
+            object.put(LANGUAGE, language.toString());
+        }
+
+        public Language getLanguage() {
+            return Language.valueOf((String) object.get(LANGUAGE));
+        }
+        
+        public Date getDateSaved() {
+            return (Date) object.get(DATE_SAVED);
+        }
+
+        public void add() {
+            BasicDBList list = (BasicDBList) person.object.get(Person.SEARCHES);
+            list.add(object);
+        }
+
+        public void remove() {
+            BasicDBList list = (BasicDBList) person.object.get(Person.SEARCHES);
+            // todo: remove
+        }
+    }
+}
