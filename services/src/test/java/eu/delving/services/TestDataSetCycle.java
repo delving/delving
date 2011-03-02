41b428094ad006f0739e78678acb70a5d3fa84d7
Sjoerd Siebinga <sjoerd@delving.eu>
2011-03-03 00:24:05 +0100

    Fixed unit test for DataSetCycle

--- services/src/test/java/eu/delving/services/TestDataSetCycle.java
+++ services/src/test/java/eu/delving/services/TestDataSetCycle.java
@@ -1,22 +1,22 @@
 /*
  * Copyright 2010 DELVING BV
  *
- *  Licensed under the EUPL, Version 1.0 or? as soon they
- *  will be approved by the European Commission - subsequent
- *  versions of the EUPL (the "Licence");
- *  you may not use this work except in compliance with the
- *  Licence.
- *  You may obtain a copy of the Licence at:
+ * Licensed under the EUPL, Version 1.1 or as soon they
+ * will be approved by the European Commission - subsequent
+ * versions of the EUPL (the "Licence");
+ * you may not use this work except in compliance with the
+ * Licence.
+ * You may obtain a copy of the Licence at:
  *
- *  http://ec.europa.eu/idabc/eupl
+ * http://ec.europa.eu/idabc/eupl
  *
- *  Unless required by applicable law or agreed to in
- *  writing, software distributed under the Licence is
- *  distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- *  express or implied.
- *  See the Licence for the specific language governing
- *  permissions and limitations under the Licence.
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied.
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
  */
 
 package eu.delving.services;
@@ -158,7 +158,7 @@ public class TestDataSetCycle {
         factory.getFileStore().createDataSetStore(MockFileStoreFactory.SPEC);
         Thread.sleep(1000);
         Assert.assertNotNull("data set info missing", clientContext.dataSetInfo);
-        Assert.assertEquals(38L, (long) clientContext.dataSetInfo.recordCount);
+        Assert.assertEquals(14141L, (long) clientContext.dataSetInfo.recordCount);
         clientContext.dataSetInfo = null;
         HttpMethod method = new GetMethod(String.format(
                 "%s/fetch/%s-sip.zip?accessKey=%s",
