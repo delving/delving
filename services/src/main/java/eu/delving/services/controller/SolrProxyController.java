055a4523f3bd49b96850e907864301e479824f18
Sjoerd Siebinga <sjoerd@delving.eu>
2011-03-29 02:14:36 +0200

    SolrProxy service now fully operational and ready for use by third-parties

--- services/src/main/java/eu/delving/services/controller/SolrProxyController.java
+++ services/src/main/java/eu/delving/services/controller/SolrProxyController.java
@@ -1,6 +1,31 @@
+/*
+ * Copyright 2011 DELVING BV
+ *
+ * Licensed under the EUPL, Version 1.1 or as soon they
+ * will be approved by the European Commission - subsequent
+ * versions of the EUPL (the "Licence");
+ * you may not use this work except in compliance with the
+ * Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://ec.europa.eu/idabc/eupl
+ *
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied.
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+
 package eu.delving.services.controller;
 
+import eu.delving.core.util.ThemeInterceptor;
+import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.log4j.Logger;
 import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,22 +60,21 @@ public class SolrProxyController {
     @RequestMapping("/api/solr/select")
     public void searchController(HttpServletRequest request, HttpServletResponse response) throws Exception {
         final String solrQueryString = request.getQueryString();
-        response.sendRedirect(solrSelectUrl + "/select?" + solrQueryString);
-//        HttpMethod method = new GetMethod(String.format("%s/select?%s", solrSelectUrl, solrQueryString));
-//        httpClient.executeMethod(method);
-//        for (Header header : method.getRequestHeaders()) {
-//            if (header.getName().equalsIgnoreCase("content-type")) {
-//                response.setContentType(method.getResponseHeader("Content-Type").getValue());
-//            }
-//            else if (header.getName().equalsIgnoreCase("server")) {
-//                //ignore
-//            }
-//            else {
-//                response.setHeader(header.getName(), header.getValue());
-//            }
-//        }
-//        response.setCharacterEncoding("UTF-8");
-//        response.getWriter().write(method.getResponseBody())); //todo add response from SolrProxy here
-//        response.getWriter().close();
+        HttpMethod method = new GetMethod(String.format("%s/select?%s", ThemeInterceptor.getTheme().getSolrSelectUrl(), solrQueryString));
+        httpClient.executeMethod(method);
+        for (Header header : method.getRequestHeaders()) {
+            if (header.getName().equalsIgnoreCase("content-type")) {
+                response.setContentType(method.getResponseHeader("Content-Type").getValue());
+            }
+            else if (header.getName().equalsIgnoreCase("server")) {
+                //ignore
+            }
+            else {
+                response.setHeader(header.getName(), header.getValue());
+            }
+        }
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(method.getResponseBodyAsString()); //todo add response from SolrProxy here
+        response.getWriter().close();
     }
 }
